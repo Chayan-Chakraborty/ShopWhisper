e603c38d71170fa1629f25a7bb224d8859937cd9
Sandeep <sandeep@Sandeeps-Laptop-4.local>
2025-05-13 05:36:35 +0530

    CHanges

--- rag_pipeline/config.py
+++ rag_pipeline/config.py
@@ -4,3 +4,90 @@ CHUNK_SIZE = 500
 CHUNK_OVERLAP = 50
 TOP_K = 5
 PDF_PATH = "/Users/sandeep/Downloads/Dummy Data Pdf - Sheet1.csv"
+FALL_BACK_DATA = {
+    "answer": {
+        "products": [
+            {
+                "ID": 1,
+                "name": "BWP Marine Plywood 18mm",
+                "type": "Plywood",
+                "properties": {
+                    "sub-category": "Waterproof",
+                    "material": "Hardwood",
+                    "waterproof": "Yes",
+                    "termite_proof": "Yes",
+                    "fire_rated": "No",
+                    "usage": "Bathrooms, Boats"
+                },
+                "wood_type": "Hardwood",
+                "thickness": "18mm",
+                "dimensions": "8x4 ft",
+                "color": "Brown",
+                "price": 1650,
+                "brand": "GreenPly",
+                "eco_friendly": "Yes",
+                "fire_resistant": "No",
+                "termite_resistant": "Yes",
+                "recommended_for": "Bathrooms, Boats",
+                "rating": 4.7,
+                "discount": "5%",
+                "stock": 50,
+                "isSponsored": True
+            },
+            {
+                "ID": 2,
+                "name": "MR Commercial Plywood 12mm",
+                "type": "Plywood",
+                "properties": {
+                    "sub-category": "Interior",
+                    "material": "Softwood",
+                    "waterproof": "No",
+                    "termite_proof": "No",
+                    "fire_rated": "No",
+                    "usage": "Furniture"
+                },
+                "wood_type": "Softwood",
+                "thickness": "12mm",
+                "dimensions": "8x4 ft",
+                "color": "Light Brown",
+                "price": 1100,
+                "brand": "Century",
+                "eco_friendly": "No",
+                "fire_resistant": "No",
+                "termite_resistant": "No",
+                "recommended_for": "Furniture",
+                "rating": 4.2,
+                "discount": "0%",
+                "stock": 120,
+                "isSponsored": True
+            },
+            {
+                "ID": 4,
+                "name": "Teak Veneer Plywood 19mm",
+                "type": "Plywood",
+                "properties": {
+                    "sub-category": "Premium",
+                    "material": "Teak",
+                    "waterproof": "Yes",
+                    "termite_proof": "Yes",
+                    "fire_rated": "Yes",
+                    "usage": "Luxury Furniture"
+                },
+                "wood_type": "Teak",
+                "thickness": "19mm",
+                "dimensions": "8x4 ft",
+                "color": "Golden",
+                "price": 2400,
+                "brand": "Kitply",
+                "eco_friendly": "Yes",
+                "fire_resistant": "Yes",
+                "termite_resistant": "Yes",
+                "recommended_for": "Luxury Furniture",
+                "rating": 4.9,
+                "discount": "15%",
+                "stock": 35,
+                "isSponsored": False
+            }
+        ]
+    }
+}
